ec87c3199f5c91ea6b5a39e346a774c712950c93
Aiden Petersen <wdaidenp@iastate.edu>
2023-03-12 15:57:34 -0500

    Modified meta html

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -8,12 +8,11 @@ export default function App({ Component }: AppProps) {
         <title>Aiden Petersen - Hardware Engineer</title>
         <meta
           name="description"
-          content="Full Stack Web Developer and TypeScript enthusiast"
+          content="Big Computer Architecture Guy - Tomasulo Enthusiast."
         />
         <meta charSet="utf-8" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-      <a class="hidden" rel="me" href="https://fosstodon.org/@mooxl"></a>
       <div class="max-w-xl my-5 mx-auto lg:mx-2 sm:mx-1 text-sm text-white font-plex leading-none tracking-wide md:my-3">
         <Component />
       </div>
